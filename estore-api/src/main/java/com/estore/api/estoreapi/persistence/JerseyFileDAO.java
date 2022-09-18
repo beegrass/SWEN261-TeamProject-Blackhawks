877dc662b55650497b90f48b9b7b38f1c40da2bc
etan Memeignton <eja9979@g.rit.edu>
2022-09-18 14:01:12 -0400

    jersey file dao constructor and fields

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/JerseyFileDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/JerseyFileDAO.java
@@ -1,13 +1,29 @@
 package com.estore.api.estoreapi.persistence;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.estore.api.estoreapi.model.Jersey;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JerseyFileDAO implements JerseyDAO {
     private static final Logger LOG  = Logger.getLogger(JerseyFileDAO.class.getName());
+    Map<Integer,Jersey> jerseys;   // Provides a local cache of the jersey objects
+                                // so that we don't need to read from the file
+                                // each time
+    private ObjectMapper objectMapper;  // Provides conversion between jersey
+                                        // objects and JSON text format written
+                                        // to the file
+    private static int nextId;  // The next Id to assign to a new jersey
+    private String filename;    // Filename to read from and write to
 
+    public JerseyFileDAO(@Value("${jerseys.file}") String filename,ObjectMapper objectMapper) throws IOException {
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+    }
     @Override
     public Jersey[] getJersey() throws IOException {
         // TODO Auto-generated method stub
