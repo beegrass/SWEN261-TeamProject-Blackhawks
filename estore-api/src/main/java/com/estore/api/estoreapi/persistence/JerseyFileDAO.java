a0bb723be3c3942f31da7dcbc19938f0446a85ef
etan Memeignton <eja9979@g.rit.edu>
2022-09-18 13:55:41 -0400

    stubbed out jerseyFileDAO

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/JerseyFileDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/JerseyFileDAO.java
@@ -1,5 +1,41 @@
 package com.estore.api.estoreapi.persistence;
 
-public class JerseyFileDAO {
-    
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import com.estore.api.estoreapi.model.Jersey;
+
+public class JerseyFileDAO implements JerseyDAO {
+    private static final Logger LOG  = Logger.getLogger(JerseyFileDAO.class.getName());
+
+    @Override
+    public Jersey[] getJersey() throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Jersey[] findJerseys(String containsText) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Jersey getJersey(int id) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Jersey createJersey(Jersey jersey) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Jersey updateJersey(Jersey jersey) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
