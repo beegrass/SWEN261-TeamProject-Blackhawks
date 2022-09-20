e519f7b88b24d079386c3c9f012a1d6d639d9509
Vince Schwartz <vas861@rit.edu>
2022-09-20 16:33:08 -0400

    Jersey model test complete and working

---
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/JerseyTest.java
@@ -0,0 +1,156 @@
+package com.estore.api.estoreapi.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The unit test suite for the Hero class
+ * 
+ * @author SWEN Faculty
+ */
+@Tag("Model-tier")
+public class JerseyTest {
+    @Test
+    public void testCtor() {
+        // Setup
+        int expected_id = 99;
+        String expected_name = "Wi-Fire";
+        int expected_number = 50;
+        double expected_price = 129.99;
+        String expected_color = "Red";
+        String expected_image = "Image.png";
+
+        // Invoke
+        Jersey jersey = new Jersey(expected_id, expected_name, expected_number, expected_price, expected_color, expected_image);
+
+        // Analyze
+        assertEquals(expected_id,jersey.getId());
+        assertEquals(expected_name,jersey.getName());
+        assertEquals(expected_number,jersey.getNumber());
+        assertEquals(expected_price,jersey.getPrice());
+        assertEquals(expected_color,jersey.getColor());
+        assertEquals(expected_image,jersey.getImage());
+    }
+
+    @Test
+    public void testName() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        int number = 50;
+        double price = 129.99;
+        String color = "Red";
+        String image = "Image.png";
+
+        Jersey jersey = new Jersey(id,name,number,price,color,image);
+
+        String expected_name = "Galactic Agent";
+
+        // Invoke
+        jersey.setName(expected_name);
+
+        // Analyze
+        assertEquals(expected_name,jersey.getName());
+    }
+
+    @Test
+    public void testNumber() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        int number = 50;
+        double price = 129.99;
+        String color = "Red";
+        String image = "Image.png";
+
+        Jersey jersey = new Jersey(id,name,number,price,color,image);
+
+        int expected_number = 20;
+
+        // Invoke
+        jersey.setNumber(expected_number);
+
+        // Analyze
+        assertEquals(expected_number,jersey.getNumber());
+    }
+
+    @Test
+    public void testPrice() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        int number = 50;
+        double price = 129.99;
+        String color = "Red";
+        String image = "Image.png";
+
+        Jersey jersey = new Jersey(id,name,number,price,color,image);
+
+        double expected_price = 100.99;
+
+        // Invoke
+        jersey.setPrice(expected_price);
+
+        // Analyze
+        assertEquals(expected_price,jersey.getPrice());
+    }
+
+    @Test
+    public void testColor() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        int number = 50;
+        double price = 129.99;
+        String color = "Red";
+        String image = "Image.png";
+
+        Jersey jersey = new Jersey(id,name,number,price,color,image);
+
+        String expected_color = "Yellow";
+
+        // Invoke
+        jersey.setColor(expected_color);
+
+        // Analyze
+        assertEquals(expected_color,jersey.getColor());
+    }
+
+    @Test
+    public void testImage() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        int number = 50;
+        double price = 129.99;
+        String color = "Red";
+        String image = "Image.png";
+
+        Jersey jersey = new Jersey(id,name,number,price,color,image);
+
+        String expected_image = "Image.jpg";
+
+        // Invoke
+        jersey.setImage(expected_image);
+
+        // Analyze
+        assertEquals(expected_image,jersey.getImage());
+    }
+
+    // @Test
+    // public void testToString() {
+    //     // Setup
+    //     int id = 99;
+    //     String name = "Wi-Fire";
+    //     String expected_string = String.format(Hero.STRING_FORMAT,id,name);
+    //     Hero hero = new Hero(id,name);
+
+    //     // Invoke
+    //     String actual_string = hero.toString();
+
+    //     // Analyze
+    //     assertEquals(expected_string,actual_string);
+    // }
+}
