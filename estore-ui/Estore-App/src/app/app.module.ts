70bf67a5b0cfe92bc6f1f7de97be5dc17b94e2e5
etan Memeignton <eja9979@g.rit.edu>
2022-10-15 19:24:24 -0400

    login page and logout created

--- estore-ui/Estore-App/src/app/app.module.ts
+++ estore-ui/Estore-App/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    CartComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
