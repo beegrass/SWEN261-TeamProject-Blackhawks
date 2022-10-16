3d22d1a43c02a165ea786e12d25c14b53a72f238
Hayden Cabral <hc6286@rit.edu>
2022-10-16 11:48:57 -0400

    Checkout framework created

--- estore-ui/Estore-App/src/app/app.module.ts
+++ estore-ui/Estore-App/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
