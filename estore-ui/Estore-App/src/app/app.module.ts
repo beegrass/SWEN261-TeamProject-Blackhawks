8babd290b81e00aff8a7d88d00b38b8e18b85a0c
Vince Schwartz <vas861@rit.edu>
2022-10-18 16:38:50 -0400

    jersey component

--- estore-ui/Estore-App/src/app/app.module.ts
+++ estore-ui/Estore-App/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
+import { JerseysComponent } from './jerseys/jerseys.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent
+    CartComponent,
+    JerseysComponent
   ],
   imports: [
     BrowserModule,
