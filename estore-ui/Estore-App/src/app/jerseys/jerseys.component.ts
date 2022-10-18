90f9cf90e4cc2ba81574f8308003cdacc664c7c4
etan Memeignton <eja9979@g.rit.edu>
2022-10-18 17:07:53 -0400

    jersey component

--- estore-ui/Estore-App/src/app/jerseys/jerseys.component.ts
+++ estore-ui/Estore-App/src/app/jerseys/jerseys.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Jersey } from '../jersey';
 import { JerseyService } from '../jersey.service';
 
 @Component({
@@ -7,22 +8,30 @@ import { JerseyService } from '../jersey.service';
   styleUrls: ['./jerseys.component.css']
 })
 export class JerseysComponent implements OnInit {
+  jerseys: Jersey[] = [];
 
   constructor(private jerseyService: JerseyService) { }
 
   ngOnInit(): void {
   }
 
-  add(): void {
-    
-  }
-
-  delete(): void {
-
-  }
-
   getJerseys(): void {
+    this.jerseyService.getJerseys()
+    .subscribe(Jerseys => this.jerseys = Jerseys);
+  }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.jerseyService.addJersey({ name } as Jersey)
+      .subscribe(Jersey => {
+        this.jerseys.push(Jersey);
+      });
+  }
+
+  delete(jersey: Jersey): void {
+    this.jerseys = this.jerseys.filter(h => h !== jersey);
+    this.jerseyService.deleteJersey(jersey.id).subscribe();
   }
 
 }
