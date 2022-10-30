994bf9b4bfecba3ccc816b58453b6d4012774253
Angela Ngo <ngo.angela0@gmail.com>
2022-10-30 15:37:21 -0400

    tested and made the cartFileDAO

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/AdminFileDAO.java
@@ -0,0 +1,16 @@
+package com.estore.api.estoreapi.persistence;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Implements the functionality for JSON file-based peristance for Customer 
+ * 
+ * {@literal @}Component Spring annotation instantiates a single instance of this
+ * class and injects the instance into other classes as needed
+ * 
+ * @author Angela Ngo
+ */
+@Component
+public class AdminFileDAO implements AdminDAO {
+    
+}
