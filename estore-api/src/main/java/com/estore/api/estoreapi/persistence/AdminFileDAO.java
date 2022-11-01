a273fe6bc0ba92b8ac3625452b7293f76c3ada5c
Angela Ngo <ngo.angela0@gmail.com>
2022-11-01 10:28:35 -0400

    add documentation to admin and customer

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/AdminFileDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/AdminFileDAO.java
@@ -11,7 +11,8 @@ import com.estore.api.estoreapi.model.Jersey;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * Implements the functionality for JSON file-based peristance for Customer 
+ * Implements the functionality for JSON file-based peristance for Admin
+ * Uses methods from the JerseyFileDAO 
  * 
  * {@literal @}Component Spring annotation instantiates a single instance of this
  * class and injects the instance into other classes as needed
@@ -51,6 +52,15 @@ public class AdminFileDAO implements AdminDAO {
     //     return true;
     // }
 
+    /**
+     * Finds the jerseys in inventory according to criteria 
+     * @param name - name of jersey 
+     * @param number - number of jersey 
+     * @param price - the price of jersey 
+     * @param color - color of the jersey
+     * @param size - size of the jersey 
+     * @return jerseys - this returns if there are jerseys found 
+     */
     @Override
     public Jersey[] findJerseysInInventory(String name, int number, double price, String color, String size) throws IOException {
        Jersey[] jerseys = jerseyFileDAO.findJerseys(name, number, price, color, size);
@@ -60,6 +70,10 @@ public class AdminFileDAO implements AdminDAO {
        return jerseys;
     }
 
+    /**
+     * @param id - id of the specific jersey 
+     * @return jersey if found null if not s
+     */
     @Override
     public Jersey getJerseyFromInventory(int id) throws IOException {
        Jersey jersey = jerseyFileDAO.getJersey(id);
@@ -69,17 +83,34 @@ public class AdminFileDAO implements AdminDAO {
        return jersey; 
     }
 
+
+    /**
+     * creates a new jersey in the inventory 
+     * @param jersey - jersey that is wanted to be created 
+     * @return newJersey - the jersey that is created based on the given one
+     */
     @Override 
     public Jersey createJerseyInInventory(Jersey jersey) throws IOException {
         Jersey newJersey = jerseyFileDAO.createJersey(jersey);
         return newJersey; 
     }
 
+    /**
+     * Updates the jersey 
+     * @param jersey - jersey to be updated 
+     * @return updatedJersey - the updated jersey after changes 
+     */
     public Jersey updateJerseyInInventory(Jersey jersey) throws IOException{
         Jersey updatedJersey = jerseyFileDAO.updateJersey(jersey);
         return updatedJersey;
     }
 
+    /**
+     * 
+     * @param id
+     * @return ifRemove - true if removed false if not
+     * @throws IOException - thrown if theres an issue with save()
+     */
     public boolean deleteJerseyFromInventory(int id) throws IOException{
         boolean ifRemove = jerseyFileDAO.deleteJersey(id); 
         return ifRemove; 
