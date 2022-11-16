279f620490120501a871b1ab126057c9dcc9aa45
Angela Ngo <ngo.angela0@gmail.com>
2022-11-16 18:28:14 -0500

    .

--- estore-ui/Estore-App/src/app/jerseys/jerseys.component.ts
+++ estore-ui/Estore-App/src/app/jerseys/jerseys.component.ts
@@ -3,6 +3,7 @@ import { Jersey } from '../jersey';
 import { JerseyService } from '../jersey.service';
 import { CartService } from 'app/cart.service';
 import { CartComponent } from 'app/cart/cart.component';
+import { LoginComponent } from 'app/login/login.component';
 @Component({
   selector: 'app-jerseys',
   templateUrl: './jerseys.component.html',
@@ -12,13 +13,17 @@ export class JerseysComponent implements OnInit {
   jerseys: Jersey[] = [];
 
   constructor(private jerseyService: JerseyService,
-    private cartComponent : CartComponent
+    private cartComponent : CartComponent,
+    private loginComponent : LoginComponent
   ) { }
 
   ngOnInit(): void {
     this.getJerseys();
   }
+// // gets the customer via the id and then cart throuhg
+//   getCurrentCustomer() : Number{
 
+//   }
   getJerseys(): void {
     this.jerseyService.getJerseys()
     .subscribe(Jerseys => this.jerseys = Jerseys);
